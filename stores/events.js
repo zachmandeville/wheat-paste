5d9daf83bef6d258ed5009bded6110e0a6faabf7
Zach <ssb.pub.key.is@ZqH7Mctu/7DNInxuwl12ECjfrAKUX2tBLq1rOldNhg0=.ed25519>
2018-05-01 23:23:15 +1200

    added main function to store

--- stores/events.js
+++ stores/events.js
@@ -3,6 +3,19 @@ module.exports = store
 function store(state, emitter){
 
 const event = require('../event.json')
-state.event = event.event
+state.event = event
+
+emitter.on('submitForm', function(form){
+  console.log('form submitted!')
+  console.log({form})
+  var archive = new DatArchive(window.location.host)
+  var data = new FormData(form)
+  var body = {}
+  for (var pair of data.entries()) body[pair[0]] = pair[1]
+  body = JSON.stringify(body, null, 2)
+  console.log({body})
+  archive.writeFile('event2.json', body)
+  console.log({state})
+})
 
 }
--- views/main.js
+++ views/main.js
@@ -1,10 +1,26 @@
+const choo = require('choo')
 const html = require('choo/html')
 const created = require('./created')
+const form = require('./form')
 
 module.exports = (state, emit) => {
-  return html`
-  <div>
-  ${created(state)}
-  </div>
-  `
+  console.log({emit})
+  return (state.event.name == '' ? showForm(state, emit) : showEvent(state, emit))
+
+  function showEvent(state){
+    return html`
+    <div>
+    ${created(state)}
+    </div>
+    `
+  }
+
+  function showForm(state, emit){
+    console.log({secondEmit: emit})
+    return html`
+      <div>
+        ${form(state, emit)}
+      </div>
+    `
+  }
 }
